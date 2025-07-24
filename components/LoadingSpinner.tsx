f1dc26c9f16c5f33a00edb492856b3d19e011320
ky1o <nixxsan1@gmail.com>
2025-07-24 11:38:26 +0300

    delete

--- components/LoadingSpinner.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const LoadingSpinner: React.FC = () => {
-  return (
-    <div className="flex justify-center items-center h-screen bg-stone-50">
-      <div className="w-16 h-16 border-4 border-lime-500 border-dashed rounded-full animate-spin"></div>
-    </div>
-  );
-};
